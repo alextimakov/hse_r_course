db9ee026e9e0d366877a761f089be050f630af8a
Alex Timakov <alextimakoff94@gmail.com>
2026-02-10 22:59:57 +0300

    changed format from R to a more convenient Rmd, updated install_packages script

--- basics/install_pack.R
+++ basics/install_pack.R
@@ -1,22 +1,34 @@
 # Скрипт для инсталляции пакетов, необходимых для курса
-required_packages <- c("car",
-                       "corrplot",
-                       "cluster",
-                       "data.table",
-                       "devtools",
-                       "dplyr",
-                       "gdata",
-                       "ggplot2",
-                       "knitr",
-                       "lavaan",
-                       "parallel",
-                       "psych",
-                       "tidyverse")
+# Includes packages for seminars 1-6 (Rmd) and general course use.
 
-existing_packages <- installed.packages()[,"Package"]
+required_packages <- c(
+  "BayesFactor",   # Seminar 3: Bayesian ANOVA
+  "car",           # Seminars 3-4: Levene, etc.
+  "caret",         # Seminars 4-6: createDataPartition, confusionMatrix
+  "cluster",       # General
+  "corrplot",      # General
+  "data.table",    # General
+  "dplyr",         # Seminars 1-6
+  "gdata",         # General
+  "ggplot2",       # Seminars 1-2, 4-6
+  "knitr",         # Rmd knitting
+  "lme4",          # Seminar 4: mixed effects
+  "MASS",          # Seminar 4: stepAIC
+  "parallel",      # General
+  "psych",         # Seminars 4-5: EFA, etc.
+  "randomForest",  # Seminars 4-6
+  "rmarkdown",     # Rmd to HTML/PDF
+  "ROSE",          # Seminar 6: imbalanced data
+  "stringr",       # Seminar 4: str_to_sentence, str_detect
+  "tidyr",         # Seminar 3: pivot_longer, etc.
+  "tidyverse"      # General (includes dplyr, tidyr, ggplot2, ...)
+)
 
+existing_packages <- installed.packages()[, "Package"]
 to_install <- required_packages[!required_packages %in% existing_packages]
 
-if (length(to_install) > 0) {for (i in to_install) {
-  install.packages(i)}
+if (length(to_install) > 0) {
+  for (i in to_install) {
+    install.packages(i, repos = "http://cran.us.r-project.org")
+  }
 }
